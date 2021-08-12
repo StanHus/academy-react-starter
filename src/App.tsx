3dff89745011806d1d0694df0c103f503b18ad33
Stan <guseletovS@gmail.com>
2021-08-12 18:33:24 +0100

    draft 2.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { greet } from "./utils/greet";
+//import { greet } from "./utils/greet";
 import PhotoEntry from "./components/photoEntry";
 
 function App(): JSX.Element {
   return (
-  <body>
+    <body>
       <header>
         <h1>My Favorite Places</h1>
       </header>
       <main>
-      <PhotoEntry
-        title= "Picture 1"
-        tags={["nature", "something"]}
-        location={"Place 1 location"}
-      />
-      <PhotoEntry
-        title= "Picture 2"
-        tags={["landscape", "something"]}
-        location={"Place 2 location"}
-      />      
+        <PhotoEntry
+          title="Picture 1"
+          tags={["nature", "something"]}
+          location={"Place 1 location"}
+        />
+        <PhotoEntry
+          title="Picture 2"
+          tags={["landscape", "something"]}
+          location={"Place 2 location"}
+        />
       </main>
-  </body>
-  )
+    </body>
+  );
 }
 
 export default App;
