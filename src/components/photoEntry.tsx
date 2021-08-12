347ad30cab7e079be72369ab969b662bc188f6ba
Stan <guseletovS@gmail.com>
2021-08-12 18:31:09 +0100

    draft 2.01

---
+++ src/components/photoEntry.tsx
@@ -0,0 +1,19 @@
+interface photoEntryProps {
+    title: string;
+    tags: string[];
+    location: string;
+  }
+  
+  function PhotoEntry(props: photoEntryProps): JSX.Element {
+    return (
+      <section>
+        <h2>{props.title}</h2>
+        <p>
+          <i>Tags: {props.tags.join(", ")}</i>
+        </p>
+        <p>{props.location}</p>
+      </section>
+    );
+  }
+  
+  export default PhotoEntry;
