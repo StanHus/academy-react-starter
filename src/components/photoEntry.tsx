6d977c791f8e5611d8f13b358bc06f4b728e5561
Stan <guseletovS@gmail.com>
2021-08-13 17:44:09 +0100

    6.3.1

--- src/components/photoEntry.tsx
+++ src/components/photoEntry.tsx
@@ -2,7 +2,7 @@ interface photoEntryProps {
   title: string;
   tags: string[];
   description: string;
-  image: string,
+  image: string;
   link: string;
 }
 
@@ -10,8 +10,7 @@ function PhotoEntry(props: photoEntryProps): JSX.Element {
   return (
     <section>
       <h2>{props.title}</h2>
- 
-      <img src={props.image} width="250" height="350" />
+      <img src={props.image} alt="movie poster" width="250" height="350" />
       <p>
         <i>Tags: {props.tags.join(", ")}</i>
       </p>
