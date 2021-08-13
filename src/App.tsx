72dac423971e49a1bfd0b71fdc59336509126a11
Stan <guseletovS@gmail.com>
2021-08-13 17:40:53 +0100

    6.3 images from web

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 //import { greet } from "./utils/greet";
 import PhotoEntry from "./components/photoEntry";
-import Thor from "./Thor.jpg";
-import Hamilton from "./hamilton.jpg";
-import Supernova from "./supernova.jpg";
+// import Thor from "./Thor.jpg";
+// import Hamilton from "./hamilton.jpg";
+// import Supernova from "./supernova.jpg";
 
 const Movie = {
   width: "1000px" as const,
@@ -52,41 +52,44 @@ function App(): JSX.Element {
         <div style={styles.Movie}>
           <PhotoEntry
             title="Thor: Ragnarok"
+            image="https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_.jpg"
             tags={["action", "comedy", "MARVEL"]}
             description="Magic boy loses his toy, befriends a green guy from their sandbox, punches his sister, and burns their house"
             link="https://goo.gl/maps/qxt9vcwFuB8JRr4x6"
           />
-          <img src={Thor} alt="thor movie poster" width="250" height="350" />
+          {/* <img src={Thor} alt="thor movie poster" width="250" height="350" /> */}
         </div>
 
         <div style={styles.Movie}>
           <PhotoEntry
             title="Hamilton"
+            image="https://m.media-amazon.com/images/M/MV5BNjViNWRjYWEtZTI0NC00N2E3LTk0NGQtMjY4NTM3OGNkZjY0XkEyXkFqcGdeQXVyMjUxMTY3ODM@._V1_.jpg"
             tags={["drama", "historic", "musical"]}
             description="A talkative dude makes plans, friends, drama, and some tradeoffs in a complex geopolitical situation"
             link="https://goo.gl/maps/r1hHSQuQ5NQb3qRv5"
           />
-          <img
+          {/* <img
             src={Hamilton}
             alt="hamilton musical poster"
             width="250"
             height="350"
-          />
+          /> */}
         </div>
 
         <div style={styles.Movie}>
           <PhotoEntry
             title="Supernova"
+            image="https://m.media-amazon.com/images/M/MV5BNDc0Y2FjZjgtOWNkYy00NjdjLWE3ZjYtZGYyNDdmYzZiZGYzXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_FMjpg_UX1000_.jpg"
             tags={["drama", "romance"]}
             description="Touching romance, but an even more powerful drama tackling the most difficult challenges in life"
             link="https://goo.gl/maps/1rf5JdZnmFzzPeLx8"
           />
-          <img
+          {/* <img
             src={Supernova}
             alt="Supernova movie poster"
             width="250"
             height="350"
-          />
+          /> */}
         </div>
       </main>
       <footer style={styles.Footer}>
