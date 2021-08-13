142d03f62327355154717c96c8c5dd4fb97247a2
Stan <guseletovS@gmail.com>
2021-08-13 02:33:08 +0100

    5.3

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,19 @@ import Thor from "./Thor.jpg";
 import Hamilton from "./hamilton.jpg";
 import Supernova from "./supernova.jpg";
 const Movie = {
-  width: "300px" as const,
-  margin: "30px auto" as const,
-  backgroundColor: "#44014C" as const,
+  width: "900px" as const,
+  margin: "50px auto" as const,
+  backgroundColor: "light grey" as const,
   textAlign: "center" as const,
   minHeight: "200px" as const,
   boxSizing: "border-box" as const,
+  border: "5px solid black" as const,
 };
 
 const Header = {
   padding: "10px 20px" as const,
   textAlign: "center" as const,
-  color: "white" as const,
+  color: "black" as const,
   fontSize: "40px" as const,
 };
 
@@ -30,38 +31,43 @@ function App(): JSX.Element {
       <header>
         <h1 style={styles.Header}>My Favorite Movies</h1>
       </header>
-      <main style={styles.Movie}>
-        <PhotoEntry
-          title="Thor: Ragnarok"
-          tags={["action", "comedy", "MARVEL"]}
-          description="Magic boy loses his toy, befriends a green guy from their sandbox, punches his sister, and burns their house"
-        />
-        <img src={Thor} alt="thor movie poster" width="200" height="350" />
+      <main>
+        <div style={styles.Movie}>
+          <PhotoEntry
+            title="Thor: Ragnarok"
+            tags={["action", "comedy", "MARVEL"]}
+            description="Magic boy loses his toy, befriends a green guy from their sandbox, punches his sister, and burns their house"
+          />
+          <img src={Thor} alt="thor movie poster" width="200" height="350" />
+        </div>
 
-        <p>Separator</p>
+        <div style={styles.Movie}>
+          <PhotoEntry
+            title="Hamilton"
+            tags={["drama", "historic", "musical"]}
+            description="A talkative dude makes plans, friends, drama, and some tradeoffs in a complex geopolitical situation"
+          />
+          <img
+            src={Hamilton}
+            alt="hamilton musical poster"
+            width="200"
+            height="350"
+          />
+        </div>
 
-        <PhotoEntry
-          title="Hamilton"
-          tags={["drama", "historic", "musical"]}
-          description="A talkative dude makes plans, friends, drama, and some tradeoffs in a complex geopolitical situation"
-        />
-        <img
-          src={Hamilton}
-          alt="hamilton musical poster"
-          width="200"
-          height="350"
-        />
-        <PhotoEntry
-          title="Supernova"
-          tags={["drama", "romance"]}
-          description="Touching romance, but an even more powerful drama tackling the most difficult challenges in life"
-        />
-        <img
-          src={Supernova}
-          alt="Supernova movie poster"
-          width="200"
-          height="350"
-        />
+        <div style={styles.Movie}>
+          <PhotoEntry
+            title="Supernova"
+            tags={["drama", "romance"]}
+            description="Touching romance, but an even more powerful drama tackling the most difficult challenges in life"
+          />
+          <img
+            src={Supernova}
+            alt="Supernova movie poster"
+            width="200"
+            height="350"
+          />
+        </div>
       </main>
     </body>
   );
