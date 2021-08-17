d452b63932f4b0aeb99bd3fd00700187c1f506b2
StanHus <guseletovS@gmail.com>
2021-08-17 21:55:47 +0100

    map done, no css

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,7 @@
 function Map(): JSX.Element {
   return (
     <iframe
+      title={"Places map"}
       src="https://www.google.com/maps/d/u/0/embed?mid=1RMoEn5EZV1JhiLtn-G5daZasVwng_0cQ"
       width="640"
       height="480"
